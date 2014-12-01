91a25956cc11cf4ae2527ad0b6750c0cc78917de
Peter af Geijerstam <peter.af.geijerstam@factor10.com>
2014-12-01 13:42:43 +0100

    First version

---
+++ FunkySerialization/FunkySerialization.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ProtoBuf;
+
+
+namespace FunkySerialization
+{
+
+ 
+
+    [ProtoContract]
+    public class Item
+    {
+        [ProtoMember(1)] 
+        public List<Aggregate> Items;
+    }
+
+    [ProtoContract]
+    public class Aggregate
+    {
+        [ProtoMember(1)] 
+        public List<SubItem> Items;
+    }
+
+    [ProtoContract]
+    public class SubItem
+    {
+        [ProtoMember(1)]
+        public long Long1;
+
+        [ProtoMember(2)]
+        public long Long2;
+
+        [ProtoMember(3)]
+        public long Long3;
+
+        [ProtoMember(4)]
+        public long Long4;
+
+        [ProtoMember(5)]
+        public long Long5;
+
+        [ProtoMember(6)]
+        public long Long6;
+    }
+
+    class FunkySerialization
+    {
+        [Test, Explicit]
+        public void ShouldSaveInPrefixStyle()
+        {
+            Item item = null;
+            var stopwatch = new Stopwatch();
+            using (
+                var fs = File.Open(@"C:\temp\huge.pbuf", FileMode.Open)
+                )
+            {
+                Console.Out.WriteLine("Loading item...");
+                Console.Out.Flush();
+                item = Serializer.Deserialize<Item>(fs);
+                Console.Out.WriteLine("Item deserialized!");
+                Console.Out.Flush();
+            }
+            
+            using (
+                var fs =
+                    File.Create(@"C:\temp\withoutprefix.pbuf"))
+            {
+                Console.Out.WriteLine("Starting serializing prefix.");
+                Console.Out.Flush();
+                stopwatch.Start();
+                Serializer.Serialize(fs,item);
+                stopwatch.Stop();
+                Console.Out.WriteLine("Done serializing without prefix. Time=" + stopwatch.ElapsedMilliseconds  + " ms");
+                Console.Out.Flush();
+            }
+
+            using (
+                var fs =
+                    File.Create(@"C:\temp\withprefix.pbuf"))
+            {
+                Console.Out.WriteLine("Starting serializing WITH prefix.");
+                Console.Out.Flush();
+                stopwatch.Reset();
+                stopwatch.Start();
+                Serializer.SerializeWithLengthPrefix(fs,item, PrefixStyle.Base128);
+                stopwatch.Stop();
+                Console.Out.WriteLine("Done serializing WITH prefix. Time=" + stopwatch.ElapsedMilliseconds + " ms");
+                Console.Out.Flush();
+            }
+
+
+
+        }
+    }
+}
